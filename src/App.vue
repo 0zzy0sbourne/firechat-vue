21c36d0542ca91a74a7a4709ca177b82b707e57c
Ozan <ozancetin6@hotmail.com>
2022-08-31 21:48:44 +0300

    login --> chat transition

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <template>
-  <h1>Hello Ozan!</h1>
+  <div class="view login" v-if="state.username === '' || state.username === null" > 
+    <form class="login-form" @submit.prevent="Login">
+      <div class="form-inner">
+        <h1>Login to FireChat</h1>
+        <label for="username">Username</label>
+        <input 
+          type="text" 
+          v-model="inputUsername"
+          placeholder="Please enter your username..." 
+        />
+        <input 
+          type="submit" 
+          value="Login" 
+        />
+      </div>
+    </form>
+  </div>  
+  <div class="view chat" v-else> 
+    Chat View
+  </div>
 </template>
 
 <script>
+import {reactive, onMounted, ref} from "vue"; 
 import db from "./db"; 
 
 export default {
   setup() {
-    return {}
+    const inputUsername = ref("")
+    const state = reactive({
+      username: "", 
+      messages: [], 
+    }); 
+
+    const Login = () => {
+      if(inputUsername.value != "" || inputUsername.value != null){
+        state.username = inputUsername.value; 
+        inputUsername.value = ""; 
+      }
+    }
+
+    return {
+      inputUsername,
+      Login, 
+      state
+    } 
   }
 }
 </script>
